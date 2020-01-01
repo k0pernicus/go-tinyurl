d25c19ab3fb6729f6af97b3a9c8563dde0d37d0d
k0pernicus <antonin.carette@gmail.com>
2020-01-01 20:11:38 +0100

    Updated dev doc

--- app/internal/tiny.go
+++ app/internal/tiny.go
@@ -6,19 +6,23 @@ import (
 	"time"
 )
 
+var (
+	// C is the internal service configuration variable
+	C Configuration
+	// DB is a temporary internal database that contains all created tiny URL and redirections
+	DB sync.Map
+)
+
+// Configuration handles all the informations for creation & launch
 type Configuration struct {
 	Host string `yaml:"host"`
 	Port string `yaml:"port"`
 }
 
+// String returns, as a string, the app as HOST:PORT
 func (c Configuration) String() string {
 	return fmt.Sprintf("%s:%s", c.Host, c.Port)
 }
-
-var C Configuration
-
-// DB is a temporary internal database that contains all created tiny URL and redirections
-var DB sync.Map
 
 // Informations store tiny object informations
 type Informations struct {
