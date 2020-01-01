514c902f00cec6825137a7afcd9f92bee4921960
k0pernicus <antonin.carette@gmail.com>
2020-01-01 17:57:13 +0100

    Renamed go source file, and added dev doc

--- app/pkg/types/service.go
+++ app/pkg/types/service.go
@@ -2,6 +2,7 @@ package types
 
 import "time"
 
+// Response structures and permits to encode the service response for any query
 type Response struct {
 	StatusCode int         `json:"status_code"`
 	Response   interface{} `json:"response"`
@@ -16,10 +17,11 @@ type CreationRequest struct {
 
 // CreationResponse is the structure, returned by the service, when creating a tiny URL
 type CreationResponse struct {
-	Message Message `json:"message"`
 	ID      string  `json:"id,omitempty"`
+	Message Message `json:"message"`
 }
 
+// ExistsResponse is a specific structure that handles the response for the exists handler
 type ExistsResponse struct {
 	Exists  bool    `json:"exists"`
 	Message Message `json:"message,omitempty"`
